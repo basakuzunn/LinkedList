9e2960256251493aba5f5994416fb57af02e181b
basakuzunn <76650722+basakuzunn@users.noreply.github.com>
2021-07-16 20:19:19 +0300

    initial commit

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -5,4 +5,21 @@ export default class LinkedList {
     this.head = null;
     this.size = 0;
   }
+  removeFirst() {
+    this.head = this.head.next;
+    this.size--;
+  }
+
+  removeLast() {
+    let tail = this.head.next;
+    let previousNode = this.head;
+
+    while (tail.next !== null) {
+      tail = tail.next;
+      previousNode = previousNode.next;
+    }
+
+    previousNode.next = null;
+    this.size--;
+  }
 }
