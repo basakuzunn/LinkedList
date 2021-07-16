60826641bf9f6d566670c67637e9ed618f7f1691
basakuzunn <76650722+basakuzunn@users.noreply.github.com>
2021-07-16 20:15:37 +0300

    initial commit

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -5,4 +5,14 @@ export default class LinkedList {
     this.head = null;
     this.size = 0;
   }
+  toArray() {
+    var arr = [];
+    let tail = this.head;
+    while (tail !== null) {
+      arr.push(tail.data);
+      tail = tail.next;
+    }
+
+    return arr;
+  }
 }
