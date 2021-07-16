04c034b7bfb70a4968ef4e220a6ee1a136c9cfb0
basakuzunn <76650722+basakuzunn@users.noreply.github.com>
2021-07-16 20:09:58 +0300

    initial commit

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -5,4 +5,16 @@ export default class LinkedList {
     this.head = null;
     this.size = 0;
   }
+  addLast(data) {
+    if (this.head === null) {
+      this.head = new Node(data);
+    } else {
+      while (this.head !== null) {
+        this.head = this.head.next;
+      }
+
+      this.head.next = new Node(data);
+    }
+    this.size++;
+  }
 }
